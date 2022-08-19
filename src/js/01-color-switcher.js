643b0f7cabd34fdb7216a652a7074d4185f3092e
Oleksandr Gryshko <alexandrgrishko@gmail.com>
2022-08-19 22:10:20 +0300

    19/08/2022_2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,8 +2,14 @@ const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 const bodyContainer = document.querySelector('body')
 let onStartClick = null;
- stopBtn.disabled = true;
+stopBtn.disabled = true;
 
+stopBtn.addEventListener('click', () => {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(onStartClisk);
+    
+});
 
 startBtn.addEventListener('click', () => {
     startBtn.disabled = true;
@@ -12,13 +18,6 @@ startBtn.addEventListener('click', () => {
         bodyContainer.style.backgroundColor = getRandomHexColor()
     }, 1000);
 
-});
-
-stopBtn.addEventListener('click', () => {
-    startBtn.disabled = false;
-    stopBtn.disabled = true;
-    clearInterval(onStartClisk);
-    
 });
 
 function getRandomHexColor() {
