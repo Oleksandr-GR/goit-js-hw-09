10370f34456591cc6862a00f33bb6e895b386043
Oleksandr-GR <100168726+Oleksandr-GR@users.noreply.github.com>
2022-08-19 18:49:52 +0000

    Deploying to gh-pages from @ Oleksandr-GR/goit-js-hw-09@254a4f8a853c98c2caccc8827d33a935b74c98f3 🚀

--- 01-color-switcher.726800e9.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.querySelector("body");e.disabled=!0,t.addEventListener("click",(function(){onStartClisk=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(onStartClisk),t.disabled=!1,e.disabled=!0}))}();
-//# sourceMappingURL=01-color-switcher.726800e9.js.map
